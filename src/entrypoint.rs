267a44e3ed6dd69a5f7419409a1f5b9af89f91f2
Parth Arora <60436959+parth1614@users.noreply.github.com>
2022-01-22 19:55:47 +0530

    Update entrypoint.rs

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -1,13 +1,13 @@
+//! Program entrypoint
+use crate::processor::Processor;
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
 };
 
-use crate::processor::Processor;
-
 entrypoint!(process_instruction);
-fn process_instruction(
+fn process_instruction<'a>(
     program_id: &Pubkey,
-    accounts: &[AccountInfo],
+    accounts: &'a[AccountInfo<'a>],
     instruction_data: &[u8],
 ) -> ProgramResult {
     Processor::process(program_id, accounts, instruction_data)
